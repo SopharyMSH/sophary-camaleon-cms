883bd2e9bb13e05f11cdf03241daa3081624befd
SopharyMSH <sopharydev@gmail.com>
2017-12-01 14:13:26 +0700

    add new post type when install theme

--- app/apps/themes/sophary_cms_default/main_helper.rb
+++ app/apps/themes/sophary_cms_default/main_helper.rb
@@ -11,6 +11,7 @@ module Themes::SopharyCmsDefault::MainHelper
   def sophary_cms_default_on_install_theme(theme)
     mycms_add_default_pages
     mycms_add_fields_to_contact_page
+    mycms_add_faqs_post_type
   end
 
   def mycms_add_default_pages
@@ -36,6 +37,24 @@ module Themes::SopharyCmsDefault::MainHelper
       file_field_group.add_field({ name: 'Phones', slug: 'phones' }, { field_key: 'text_box', required: true, multiple: true, default_value: '070-xxxx-xxxx'})
       file_field_group.add_field({ name: 'Address', slug: 'address' }, { field_key: 'editor', required: true, default_value: 'Lorem Ipsum dolor sit amet, consectetur adipiscing elit.' })
       file_field_group.add_field({ name: 'Embed Map Url', slug: 'embed-map-url' }, { field_key: 'url', required: true, default_value: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d250151.15120764848!2d104.75010047461711!3d11.579666934905125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3109513dc76a6be3%3A0x9c010ee85ab525bb!2sPhnom+Penh!5e0!3m2!1sen!2skh!4v1507699269146' })
+    end
+  end
+
+  def mycms_add_faqs_post_type
+    if current_site.the_post_type('faqs').blank?
+      faqs = current_site.post_types.create(name: 'Faqs', slug: 'faqs')
+      options = {
+        has_category: false,
+        has_content: true,
+        has_tags: false,
+        has_summary: false,
+        has_comments: false,
+        has_picture: false,
+        has_template: false,
+        has_keywords: false,
+        contents_route_format: 'post_of_posttype'
+      }
+      faqs.set_meta('_default', options)
     end
   end
 
